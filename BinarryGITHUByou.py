cf2e9f83e2b240e43aaf8fdff5f646e80c106049
mirai-work <mfkensub@gmail.com>
2026-07-20 13:04:44 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- BinarryGITHUByou.py
+++ BinarryGITHUByou.py
@@ -2,7 +2,7 @@ import pyxel
 import random
 import math
 
-# --- 定数設定 ---
+# --- 定数設定 ---追加
 GAMEPAD_DPAD_UP = pyxel.GAMEPAD1_BUTTON_DPAD_UP
 GAMEPAD_DPAD_DOWN = pyxel.GAMEPAD1_BUTTON_DPAD_DOWN
 GAMEPAD_DPAD_LEFT = pyxel.GAMEPAD1_BUTTON_DPAD_LEFT
